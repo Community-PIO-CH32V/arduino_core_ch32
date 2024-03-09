02363b1ebcf34963fdd3872d02aa60cb5380bfdd
microcai <microcaicai@gmail.com>
2024-03-09 11:48:56 +0800

    Systick->CNT is up couting actually
    Systick->CNT is up couting and wrap to zero when reached Systick->CMP.

--- cores/arduino/ch32/clock.c
+++ cores/arduino/ch32/clock.c
@@ -112,9 +112,9 @@ uint32_t getCurrentMicros(void)
            tms = (tms << 32) + *((__IO uint32_t *)SYSTICK_CMPL) + 1;     
 
   if (m1 != m0) {
-    return (m1 * 1000 + ((tms - u1) * 1000) / tms);
+    return (m1 * 1000 + ((u1) * 1000) / tms);
   } else {
-    return (m0 * 1000 + ((tms - u0) * 1000) / tms);
+    return (m0 * 1000 + ((u0) * 1000) / tms);
   }
 }
 
